75e196bff75596aaff3542e40bf9e3d47e15dae0
SLey3 <ghub4127@gmail.com>
2024-02-23 17:45:54 -0800

    [skip render] main page completion advanced + configurations added to tailwind css config + updated .gitignore

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,17 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("./static/node_modules/tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./static/src/**/*.js"
+    "./static/src/**/*.js",
+    "./static/node_modules/flowbites/**/*.js"
   ],
   theme: {
+    container: {
+      padding: '1.5rem'
+    },
     extend: {
-      fontFamily : {
-        protest : ['"Protest Riot"', "sans-serif"]
+      fontFamily: {
+        dancing: [
+          '"Dancing Script", cursive', {
+            fontVariationSettings: '"wght" 500'
+          }
+        ],
+        ptsans: [
+          '"PT Sans", sans-serif', {
+            fontVariationSettings: '"wght" 400'
+          }
+        ],
+        notoserif: [
+          ['"Noto Serif"', defaultTheme.fontFamily.serif], {
+            fontVariationSettings: '"wdth" 100'
+          }
+        ]
       }
     },
+    fontWeight: {
+      '530' : '530'
+    }
   },
-  plugins: [],
+  plugins: [
+    require("./static/node_modules/flowbite/plugin")
+  ],
 }
 
