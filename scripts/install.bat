c119d2a14fa7a0dfc7905af417715d93309dcfa2
SLey3 <ghub4127@gmail.com>
2024-02-28 19:26:37 -0800

    [skip render] batch file fixed

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,49 +1,59 @@
 @echo off
 
-echo "[SYSTEM] Running install.sh"
+echo "[SYSTEM] Running install.bat"
 echo "[SYSTEM] Installing python dependencies..."
-IF ! "command" "-v" "pip" (
-  echo "-e" "[31m][ERROR] pip is not installed. Please install pip before running this script.[0m"
-  exit "1"
+
+pip --version 2>NUL
+IF errorlevel 1 (
+  echo "ESC[31m Pip is not installed. Please install pip before running this script. ESC[0m"
+  exit 1
 )
-pip "install" "-r" "requirements.txt"
-pip "install" "-r" "dev-requirements.txt"
-echo "-e" "[32m[SYSTEM] Python dependencies installed[0m"
+
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
+
+echo "ESC[32m[SYSTEM] Python dependencies installed ESC[0m"
 echo "[SYSTEM] Installing node dependencies..."
+
 cd "site"
-IF ! "command" "-v" "npm" (
-  echo "-e" "[31m[ERROR] npm is not installed. Please install npm before running this script.[0m"
-  exit "1"
+
+call npm --version 2>NUL
+IF errorlevel 1 (
+  echo "ESC[31m[ERROR] npm is not installed. Please install npm before running this script. ESC[0m"
+  exit 1
 )
-IF ! "command" "-v" "node" (
-  echo "-e" "[31m[ERROR] node is not installed. Please install node before running this script.[0m"
-  exit "1"
+
+call node --version 2>NUL
+IF errorlevel 1 (
+  echo "ESC[31m[ERROR] node is not installed. Please install node before running this script. ESC[0m"
+  exit 1
 )
-IF "-d" "%CD%\static\node_modules" (
+
+IF exist "%CD%\static\node_modules" (
   echo "[SYSTEM] Node modules directory installed. Manually checking for required modules... ."
   echo "[SYSTEM] Checking for tailwindcss..."
-  IF "-d" "%CD%\static\node_modules\tailwindcss" (
-    echo "-e" "[32m[SYSTEM] Tailwindcss is installed[0m"
+  IF exist "%CD%\static\node_modules\tailwindcss" (
+    echo "ESC[32m[SYSTEM] Tailwindcss is installed ESC[0m"
   ) ELSE (
-    echo "-e" "[31m[SYSTEM] Tailwindcss is not installed. Installing tailwindcss...[0m"
-    npm "install" "tailwindcss" "--prefix" "%CD%\static"
+    echo "ESC[31m[SYSTEM] Tailwindcss is not installed. Installing tailwindcss... ESC[0m"
+    call npm install tailwindcss --prefix %CD%\static
     echo "-e" "[32m[SYSTEM] Tailwindcss installed[0m"
   )
   echo "[SYSTEM] Checking for flowbite..."
-  IF "-d" "%CD%\static\node_modules\flowbite" (
-    echo "-e" "[32m[SYSTEM] Flowbite is installed[0m"
+  IF exist "%CD%\static\node_modules\flowbite" (
+    echo  "ESC[32m[SYSTEM] Flowbite is installed ESC[0m"
   ) ELSE (
-    echo "-e" "[31m[SYSTEM] Flowbite is not installed. Installing flowbite...[0m"
-    npm "install" "flowbite" "--prefix" "%CD%\static"
-    echo "-e" "[32m[SYSTEM] Flowbite installed[0m"
+    echo "ESC[31m[SYSTEM] Flowbite is not installed. Installing flowbite... ESC[0m"
+    call npm install flowbite --prefix %CD%\static
+    echo "ESC[32m[SYSTEM] Flowbite installed ESC[0m"
   )
-  echo "-e" "[32m[SYSTEM] Node dependencies installed, installation complete [0m"
-  exit "0"
+  echo "ESC[32m[SYSTEM] Node dependencies installed, installation complete ESC[0m"
+  exit 0
 ) ELSE (
-  npm "install"
-  echo "-e" "[32m[SYSTEM] Node dependencies installed[0m"
+  call npm install
+  echo "ESC[32m[SYSTEM] Node dependencies installed ESC[0m"
 )
 echo "[SYSTEM] Moving node_modules to static folder"
 echo "[SYSTEM] Running on a non-Windows system"
-mv "%CD%\node_modules" "%CD%\static"
-echo "-e" "[32m[SYSTEM] Installation complete[0m"
+move "%CD%\node_modules" "%CD%\static"
+echo "ESC[32m[SYSTEM] Installation complete ESC[0m"
